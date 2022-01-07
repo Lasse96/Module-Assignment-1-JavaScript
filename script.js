d648efe6251ebe7e385ec024e5ac460114f97538
Lasse96 <91594348+Lasse96@users.noreply.github.com>
2022-01-07 14:58:10 +0100

    done

---
+++ script.js
@@ -0,0 +1,33 @@
+// 1
+var number = 10;
+var boolean = true;
+var text = "Hei"
+
+console.log(number);
+console.log(boolean);
+console.log(text);
+
+// 2
+var fname = "Lasse ";
+var lname = "Strand";
+var flname;
+
+flname = (fname + lname)
+console.log(flname);
+
+// 3
+
+var x = typeof "frog";
+console.log("The type of frog is " + x);
+
+// 4
+
+var orderHasShipped = true;
+if (orderHasShipped == true){
+    console.log("The order shipped");
+} else console.log("The order did not ship");
+
+// 5
+
+for ( var v = 7; v <=13; v++)
+console.log(v);
